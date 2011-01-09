8382a143c80b487fedd0b5385079c47cb96aad85
pbodsk <me@peterbodskov.dk>
2011-01-09 18:05:43 +0100

    just a bit of housekeeping

--- time_slice.rb
+++ time_slice.rb
@@ -3,18 +3,9 @@ class TimeSlice
   def initialize(input_line)
     #00:21:29,621
     @hours, @minutes, @seconds, @miliseconds = input_line.scan(/\d+/)
-#    puts "hours: #{@hours}, minutes: #{@minutes}, seconds: #{@seconds}, miliseconds: #{@miliseconds}"
-#    @hours = input_line[0,2].to_i
-#    @minutes = input_line[3,2].to_i
-#    @seconds = input_line[6,2].to_i
-#    @miliseconds = input_line[9,3].to_i
   end
     
   def to_s
-#    parse_miliseconds
-#    parse_seconds
-#    parse_minutes
-#    parse_hours
     "#{@hours}:#{@minutes}:#{@seconds},#{@miliseconds}"
   end
   
